a00407f89a200c2f123bcb7a46f95d5b8682146b
S1lkBeard <jahwrath@yahoo.com>
2023-09-24 17:27:59 +0300

    Переименованы переменные

--- src/main/java/s1lkbeard/home/Task_1_2.java
+++ src/main/java/s1lkbeard/home/Task_1_2.java
@@ -15,8 +15,8 @@ public class Task_1_2
         System.out.println();
         System.out.println("Значение функции f(x) = (sqrt(exp(2.2 * x))) - abs(sin((PI * x)/(x + 2.0 / 3))) + 1.7 равно:");
         for (double x = A; x < B; x=x+(B-A)/N) {
-            double task01 = (sqrt(exp(2.2 * x))) - abs(sin((PI * x)/(x + 2.0 / 3))) + 1.7;
-            System.out.print(new DecimalFormat("#0.000").format(task01));
+            double eq1 = (sqrt(exp(2.2 * x))) - abs(sin((PI * x)/(x + 2.0 / 3))) + 1.7;
+            System.out.print(new DecimalFormat("#0.000").format(eq1));
             System.out.print(" при x = ");
             System.out.print(new DecimalFormat("#0.000").format(x));
             System.out.println();
