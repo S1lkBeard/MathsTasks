29d46eba0f702b0f4250a75e36966cbdedb90d79
S1lkBeard <jahwrath@yahoo.com>
2023-09-24 17:41:03 +0300

    Изменено форматирование вывода результатов
    В Task_1_2.java расчет значения приращения цикла вынесено за пределы цикла

--- src/main/java/s1lkbeard/home/Task_1_1.java
+++ src/main/java/s1lkbeard/home/Task_1_1.java
@@ -19,21 +19,11 @@ public class Task_1_1
         double eq4 = (sqrt(x) * sin(pow(x, 2) / 2 )- 1.3)/(pow(x, 0.2) + exp(3 * x) + abs(cos(x)));
         double eq5 = (sqrt(exp(abs(pow(sin(x), 3))))) + 2 * log(3 * x) - 1.0 / 9;
 
-        System.out.println("При x = " + x + " значение функции f(x):");
-        System.out.println();
-        System.out.print("1) (sqrt(exp(2.2 * x))) - abs(sin((PI * x)/(x + 2.0 / 3))) + 1.7 = ");
-        System.out.println(new DecimalFormat("#0.000").format(eq1));
-        System.out.println();
-        System.out.print("2) sqrt(pow(pow(x, 4), 0.2) + pow(pow(x, (4 - x)), 0.2)) + log(abs(x - 20.5)) = ");
-        System.out.println(new DecimalFormat("#0.000").format(eq2));
-        System.out.println();
-        System.out.print("3) (1.0 / 7 + log(sqrt(x))) * exp(sqrt(x - 2)) = ");
-        System.out.println(new DecimalFormat("#0.000").format(eq3));
-        System.out.println();
-        System.out.print("4) (sqrt(x) * sin(pow(x, 2) / 2 )- 1.3)/(pow(x, 0.2) + exp(3 * x) + abs(cos(x))) = ");
-        System.out.println(new DecimalFormat("#0.000").format(eq4));
-        System.out.println();
-        System.out.print("5) (sqrt(exp(abs(pow(sin(x), 3))))) + 2 * log(3 * x) - 1.0 / 9 = ");
-        System.out.println(new DecimalFormat("#0.000").format(eq5));
+        System.out.printf("При x = " + x + " значение функции f(x): %n %n");
+        System.out.printf("1) (sqrt(exp(2.2 * x))) - abs(sin((PI * x)/(x + 2.0 / 3))) + 1.7 = %f %n %n", eq1);
+        System.out.printf("2) sqrt(pow(pow(x, 4), 0.2) + pow(pow(x, (4 - x)), 0.2)) + log(abs(x - 20.5)) = %f %n %n", eq2);
+        System.out.printf("3) (1.0 / 7 + log(sqrt(x))) * exp(sqrt(x - 2)) = %f %n %n", eq3);
+        System.out.printf("4) (sqrt(x) * sin(pow(x, 2) / 2 )- 1.3)/(pow(x, 0.2) + exp(3 * x) + abs(cos(x))) = %f %n %n", eq4);
+        System.out.printf("5) (sqrt(exp(abs(pow(sin(x), 3))))) + 2 * log(3 * x) - 1.0 / 9 = %f %n %n", eq5);
     }
 }
--- src/main/java/s1lkbeard/home/Task_1_2.java
+++ src/main/java/s1lkbeard/home/Task_1_2.java
@@ -8,17 +8,19 @@ public class Task_1_2
 {
     public static void main(String[] args )
     {
-        byte N=10;
-        double A=0.55;
-        double B=1.0;
+        byte N = 10;
+        double A = 0.55;
+        double B = 1.0;
+        double step = (B - A) / N;
 
         System.out.println();
         System.out.println("Значение функции f(x) = (sqrt(exp(2.2 * x))) - abs(sin((PI * x)/(x + 2.0 / 3))) + 1.7 равно:");
-        for (double x = A; x < B; x=x+(B-A)/N) {
+        for (double x = A; x < B; x = x + step) {
             double eq1 = (sqrt(exp(2.2 * x))) - abs(sin((PI * x)/(x + 2.0 / 3))) + 1.7;
+            System.out.print("x = ");
+            System.out.print(new DecimalFormat("#0.000").format(x) + " f(x) = ");
             System.out.print(new DecimalFormat("#0.000").format(eq1));
-            System.out.print(" при x = ");
-            System.out.print(new DecimalFormat("#0.000").format(x));
+            System.out.printf(" f(x) = %f %n", eq1);
             System.out.println();
         }
     }
