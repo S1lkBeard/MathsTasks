7fc34834bd436bde23a4a387e75c9996729c8039
S1lkBeard <101265422+S1lkBeard@users.noreply.github.com>
2023-10-22 19:02:37 +0300

    Изменение нейминга задач (#11)

--- src/main/java/s1lkbeard/home/Task_1_3_1_1.java
+++ src/main/java/s1lkbeard/home/Task_1_3_1_1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 import static java.lang.Math.*;
 
-public class Task_1_3_1 {
+public class Task_1_3_1_1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         //количество слоёв
--- src/main/java/s1lkbeard/home/Task_1_3_1_2.java
+++ src/main/java/s1lkbeard/home/Task_1_3_1_2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 import static java.lang.Math.*;
 
-public class Task_1_3_2 {
+public class Task_1_3_1_2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         //количество прямоугольных треугольников
