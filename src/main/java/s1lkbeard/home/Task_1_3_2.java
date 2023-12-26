49d05d7ac0ecbd958f8f71215bc00af7f0f548b3
S1lkBeard <101265422+S1lkBeard@users.noreply.github.com>
2023-12-26 19:00:58 +0300

    Исправление ошибок, внесение правок, добавление тестов (#18)

--- src/main/java/s1lkbeard/home/Task_1_3_2.java
+++ src/main/java/s1lkbeard/home/Task_1_3_2.java
@@ -8,20 +8,26 @@ public class Task_1_3_2 {
         double X = 0.5;
         int n = 20;
 
-        double res = 1;
-
-        for (double k = 1; k < n + 1; k++) {
-            res = (getEq(res, X, k));
-            if (k == 20) {
-                double finalRes = sqrt(X) * log(X) - res;
-                System.out.printf("f(k) = sqrt(X) * log(X) - П(от_k=1_до_n) ((pow((pow(X, (k - 1)) / (k + 1.0 / 3.0)), 1.0 / k) - 0.5)) = %.4f %n %n", finalRes);
-            }
+        //Получение значения уравнения на 1 итерации
+        double res = (getEq(X, 1));
+        //Получение и перемножение значений уравнений с 2 по 20 итерацию
+        for (double k = 2; k <= n; k++) {
+            res = res * (getEq(X, k));
         }
+        //Последнее действие
+        res = getfinalEq(X, res);
+        System.out.printf("f(k) = sqrt(X) * log(X) - П(от_k=1_до_n) ((pow((pow(X, (k - 1)) / (k + 1.0 / 3.0)), 1.0 / k) + 0.5)) = %.4f %n %n", res);
     }
 
-    public static double getEq(double res, double X, double k) {
-        res = res * (pow((pow(X, (k - 1)) / (k + 1.0 / 3.0)), 1.0 / k) - 0.5);
+    public static double getEq(double X, double k) {
+        double res = (pow((pow(X, (k - 1)) / (k + 1.0 / 3.0)), 1.0 / k) + 0.5);
 
         return res;
+    }
+
+    public static double getfinalEq(double X, double res) {
+        double finalRes = sqrt(X) * log(X) - res;
+
+        return finalRes;
     }
 }
